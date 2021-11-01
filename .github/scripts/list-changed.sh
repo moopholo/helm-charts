739fd0307ab2bbcb72032abf02d164b3ad8fc261
albertrdixon <adixon415@gmail.com>
2021-11-01 13:35:15 -0700

    add tag job

--- .github/scripts/list-changed.sh
+++ .github/scripts/list-changed.sh
@@ -4,18 +4,28 @@ export PROJECT_DIR="${GITHUB_WORKSPACE:-$(git rev-parse --show-toplevel)}"
 source "$PROJECT_DIR/.github/scripts/utils.sh"
 
 function charts_with_changes_json_array() {
+  declare -a changes
   declare -a charts
-  for c in $(ct list-changed --config "$PROJECT_DIR/.github/chart-testing.yml"); do
-    charts+=("${c##charts/}")
+
+  if [[ $# -eq 0 ]]; then
+    mapfile -t changes < <(ct list-changed --config "$PROJECT_DIR/.github/chart-testing.yml")
+  else
+    changes=("$@")
+  fi
+
+  for c in "${changes[@]}"; do
+    if [[ $c =~ ^charts ]]; then
+      charts+=("$(echo "$c" | cut -d/ -f2)")
+    fi
   done
 
   # shellcheck disable=SC2068
   echo "[\"$(join_by '","' ${charts[@]})\"]"
 }
 
-changed="$(charts_with_changes_json_array)"
+changed="$(charts_with_changes_json_array "$@")"
 echo "::set-output name=matrix::{\"chart-name\":$changed}"
-if [[ "$changed" = '[""]' ]]; then
+if [[ $changed == '[""]' ]]; then
   echo "::set-output name=changes::false"
 else
   echo "::set-output name=changes::true"
