7e6882857b62f74649c625ac76f210b05cc32d88
Albert Dixon <adixon415@gmail.com>
2021-11-01 17:31:38 -0700

    fix(ci): no v prefix on tag (#45)

--- .github/scripts/release-charts.sh
+++ .github/scripts/release-charts.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export PROJECT_DIR="${GITHUB_WORKSPACE:-$(git rev-parse --show-toplevel)}"
 
+# shellcheck disable=SC1091
 source "$PROJECT_DIR/.github/scripts/utils.sh"
 
 pushd "$PROJECT_DIR" 2>/dev/null || exit 1
@@ -13,7 +14,7 @@ mkdir -p "$PROJECT_DIR/.helm"
 
 cr_config="$PROJECT_DIR/.github/chart-release.yml"
 
-for chart in $PROJECT_DIR/charts/*; do
+for chart in "$PROJECT_DIR"/charts/*; do
   if [[ -f "$chart/Chart.yaml" ]]; then
     echo "---> Packaging chart $(basename "$chart")"
     cr package "$chart" --config "$cr_config"
